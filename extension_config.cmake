48064e058c238acbb827a409b48b456dacc90b5b
Sam Ansmink <samansmink@hotmail.com>
2024-08-15 12:23:29 +0200

    ci debug run crgo tree

--- extension_config.cmake
+++ extension_config.cmake
@@ -9,12 +9,12 @@ duckdb_extension_load(delta
 # Build the httpfs extension to test with s3/http
 duckdb_extension_load(httpfs)
 
-# Build the azure extension to test with azure
-duckdb_extension_load(azure
-        LOAD_TESTS
-        GIT_URL https://github.com/duckdb/duckdb_azure
-        GIT_TAG 49b63dc8cd166952a0a34dfd54e6cfe5b823e05e
-)
+## Build the azure extension to test with azure
+#duckdb_extension_load(azure
+#        LOAD_TESTS
+#        GIT_URL https://github.com/duckdb/duckdb_azure
+#        GIT_TAG 49b63dc8cd166952a0a34dfd54e6cfe5b823e05e
+#)
 
 # Build the aws extension to test with credential providers
 duckdb_extension_load(aws
