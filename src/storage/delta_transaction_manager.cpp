9a4d13d102a47b056342f6faeb6b083e6a2e23f9
Sam Ansmink <samansmink@hotmail.com>
2025-07-31 17:49:48 +0200

    fix: catch transaction exceptions properly

--- src/storage/delta_transaction_manager.cpp
+++ src/storage/delta_transaction_manager.cpp
@@ -18,7 +18,11 @@ Transaction &DeltaTransactionManager::StartTransaction(ClientContext &context) {
 
 ErrorData DeltaTransactionManager::CommitTransaction(ClientContext &context, Transaction &transaction) {
 	auto &delta_transaction = transaction.Cast<DeltaTransaction>();
-	delta_transaction.Commit(context);
+    try {
+        delta_transaction.Commit(context);
+    } catch (std::exception &ex) {
+        return ErrorData(ex);
+    }
 	lock_guard<mutex> l(transaction_lock);
 	transactions.erase(transaction);
 	return ErrorData();
